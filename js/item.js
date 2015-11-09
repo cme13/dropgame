d0945da52c8f5db4ee7237bffd56f377745e63c7
cme13 <cme13@students.pti.edu>
2015-11-09 14:32:50 -0500

    fisrst 31
    fisrst 31

--- js/item.js
+++ js/item.js
@@ -2,11 +2,15 @@
  * The Item class is a blueprint for each game element we generate
  * @author  John Doe
  * @version 1.0, Nov 2015
+ *
+ *@param cl   which class to use to style it
+ *@param xpos     int  -  horizontal starting position
+ *@param ypos      int  -  vertical starting position
  */
-function Item(){
+function Item(cl, xpos, ypos){
 	//properties to let each item store its own x/y position
-	this.x = Math.floor(Math.random()*720); 
-	this.y = Math.floor(Math.random()*500);
+	this.x = xpos; 
+	this.y = ypos;
 	//property that keeps track of the item's visual
 	this.item_on_page;
 	/**
@@ -14,7 +18,7 @@ function Item(){
 	 */ 
 	 this.create = function(){
 		 this.item_on_page = document.createElement("div");
-		 this.item_on_page.className = "raindrop";
+		 this.item_on_page.className = cl;
 		 //use this object's x & y var's to position it in CSS
 		 this.item_on_page.style.left = this.x + "px";
 		 this.item_on_page.style.top = this.y + "px";
@@ -25,7 +29,19 @@ function Item(){
 	 * The destroy method removes the item from the game
 	 */ 
 	 this.destroy = function(){
-		 
+         //assign an animating GIF where the drop is
+         var newsplash = document.createElement("img");
+         newsplash.src="img/splash.gif?"+Math.random();
+         newsplash.style.position="absolute";
+         newsplash.style.left=this.x+"px";
+         newsplash.style.top=this.y+"px";
+         document.body.appendChild(newsplash);
+         //figure out this drop's index num
+         var index_num = drop_array.indexOf(this);
+          // to remove an item from array: splice(index,howMany);
+         drop_array.splice(0,1);
+		  //remove div on the page from the child of the body
+		 document.body.removeChild(this.item_on_page);
 	 }//end function destroy()
 
 }//end the Item class
